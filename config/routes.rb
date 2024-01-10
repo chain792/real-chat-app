e561bf498f7026d3073723e9af2af5d5b262acf3
itoshusu <sopfantasy@yahoo.co.jp>
2024-01-11 08:32:52 +0900

    deviseとmailhogの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  authenticated do
+    root 'home#index', as: :authenticated_root
+  end
+
+  devise_scope :user do
+    root "users/sessions#new"
+  end
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    unlocks: 'users/unlocks'
+  }
+
+  get 'home' => 'home#index'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
